900ce5f39dd8230181d60490e4fc1e8bf185fed9
opilane <opilane@HP-CZC6297BB7>
2020-09-25 13:26:06 +0300

    sdelal snegovika

--- jasska2/jasska2/lund.xaml.cs
+++ jasska2/jasska2/lund.xaml.cs
@@ -12,30 +12,35 @@ namespace jasska2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class lund : ContentPage
     {
-        BoxView lglaz,pglaz,vedro,verh,centr,niz;
+        BoxView lglaz,pglaz,vedro,vedro2,verh,centr,niz;
         public lund()
         {
             AbsoluteLayout abs = new AbsoluteLayout();
-            //ведро на голову
-            vedro = new BoxView { Color = Color.DarkGray, CornerRadius = 0 };
-            AbsoluteLayout.SetLayoutBounds(vedro, new Rectangle(0.5, 0.05, 60, 60));
-            AbsoluteLayout.SetLayoutFlags(vedro, AbsoluteLayoutFlags.PositionProportional);
-            abs.Children.Add(vedro);
-            //левый глаз
-            lglaz = new BoxView { Color = Color.Black, CornerRadius = 60 };
-            AbsoluteLayout.SetLayoutBounds(lglaz, new Rectangle(0.5, 0.15, 10, 10));
-            AbsoluteLayout.SetLayoutFlags(lglaz, AbsoluteLayoutFlags.PositionProportional);
-            abs.Children.Add(lglaz);
-            //правый глаз
-            pglaz = new BoxView { Color = Color.Black, CornerRadius = 60 };
-            AbsoluteLayout.SetLayoutBounds(pglaz, new Rectangle(0.5, 0.15, 10, 10));
-            AbsoluteLayout.SetLayoutFlags(pglaz, AbsoluteLayoutFlags.PositionProportional);
-            abs.Children.Add(pglaz);
             //верхняя часть
             verh = new BoxView { Color = Color.SkyBlue, CornerRadius = 60 };
             AbsoluteLayout.SetLayoutBounds(verh, new Rectangle(0.5, 0.15, 100, 100));
             AbsoluteLayout.SetLayoutFlags(verh, AbsoluteLayoutFlags.PositionProportional);
             abs.Children.Add(verh);
+            //ведро на голову
+            vedro = new BoxView { Color = Color.Black, CornerRadius = 0 };
+            AbsoluteLayout.SetLayoutBounds(vedro, new Rectangle(0.5, 0.043, 60, 60));
+            AbsoluteLayout.SetLayoutFlags(vedro, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(vedro);
+            //ведро2
+            vedro2 = new BoxView { Color = Color.Black, CornerRadius = 0 };
+            AbsoluteLayout.SetLayoutBounds(vedro2, new Rectangle(0.5, 0.13, 120, 10));
+            AbsoluteLayout.SetLayoutFlags(vedro2, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(vedro2);
+            //левый глаз
+            lglaz = new BoxView { Color = Color.Black, CornerRadius = 60 };
+            AbsoluteLayout.SetLayoutBounds(lglaz, new Rectangle(0.45, 0.17, 10, 10));
+            AbsoluteLayout.SetLayoutFlags(lglaz, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(lglaz);
+            //правый глаз
+            pglaz = new BoxView { Color = Color.Black, CornerRadius = 60 };
+            AbsoluteLayout.SetLayoutBounds(pglaz, new Rectangle(0.55, 0.17, 10, 10));
+            AbsoluteLayout.SetLayoutFlags(pglaz, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(pglaz);
             //центральная часть
             centr = new BoxView { Color = Color.SkyBlue, CornerRadius = 60 };
             AbsoluteLayout.SetLayoutBounds(centr, new Rectangle(0.5, 0.38, 130, 130));
